380d38b5ee00e95a53cd2b1bfb52ffc7962d3422
James Anderson <james.anderson2@btinternet.com>
2025-07-13 21:11:14 +0100

    Retreive albums from itunes

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,17 +1,12 @@
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia.MusicStore.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Avalonia.MusicStore.ViewModels;
 
 public partial class MusicStoreViewModel : ViewModelBase
 {
-    public MusicStoreViewModel()
-    {
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
-    }
-
     [ObservableProperty] public partial string? SearchText { get; set; }
 
     [ObservableProperty] public partial bool IsBusy { get; private set; }
@@ -19,4 +14,25 @@ public partial class MusicStoreViewModel : ViewModelBase
     [ObservableProperty] public partial AlbumViewModel? SelectedAlbum { get; set; }
 
     public ObservableCollection<AlbumViewModel> SearchResults { get; } = [];
+
+    partial void OnSearchTextChanged(string? value)
+    {
+        _ = DoSearch(SearchText);
+    }
+
+    private async Task DoSearch(string? term)
+    {
+        IsBusy = true;
+        SearchResults.Clear();
+
+        var albums = await Album.SearchAsync(term);
+
+        foreach (var album in albums)
+        {
+            var vm = new AlbumViewModel(album);
+            SearchResults.Add(vm);
+        }
+
+        IsBusy = false;
+    }
 }
