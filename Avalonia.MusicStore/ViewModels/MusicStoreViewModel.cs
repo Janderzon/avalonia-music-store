fceac2a328dd71319b33936daf554bac0c701002
James Anderson <james.anderson2@btinternet.com>
2025-07-13 20:43:57 +0100

    Add fake data

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,22 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Avalonia.MusicStore.ViewModels;
 
-public class MusicStoreViewModel
+public partial class MusicStoreViewModel : ViewModelBase
 {
-    
+    public MusicStoreViewModel()
+    {
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
+
+    [ObservableProperty] public partial string? SearchText { get; set; }
+
+    [ObservableProperty] public partial bool IsBusy { get; private set; }
+
+    [ObservableProperty] public partial AlbumViewModel? SelectedAlbum { get; set; }
+
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = [];
 }
