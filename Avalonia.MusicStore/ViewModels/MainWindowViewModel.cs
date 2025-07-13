cd4e8a301174e1b57ff9d0e4ba1b166f7f69cbe5
James Anderson <james.anderson2@btinternet.com>
2025-07-13 22:01:19 +0100

    Add albums to users collection

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Threading.Tasks;
 using Avalonia.MusicStore.Messages;
@@ -8,6 +9,8 @@ namespace Avalonia.MusicStore.ViewModels;
 
 public partial class MainWindowViewModel : ObservableObject
 {
+    public ObservableCollection<AlbumViewModel> Albums { get; } = [];
+
     public MainWindowViewModel()
     {
         // ViewModel initialization logic.
@@ -18,5 +21,9 @@ public partial class MainWindowViewModel : ObservableObject
     {
         // Send the message to the previously registered handler and await the selected album
         var album = await WeakReferenceMessenger.Default.Send(new PurchaseAlbumMessage());
+        if (album is not null)
+        {
+            Albums.Add(album);
+        }
     }
 }
